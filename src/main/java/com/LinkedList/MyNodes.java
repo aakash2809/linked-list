436e06d91a3d87f12e2cabbae05a40a02a664066
aakash2809 <aakashrajak289@gmail.com>
2020-12-07 08:20:16 +0530

    Added : Add 3 number to top of Linked List

---
+++ src/main/java/com/LinkedList/MyNodes.java
@@ -0,0 +1,30 @@
+package com.LinkedList;
+
+public class MyNodes<K> implements INode<K> {
+	private K key;
+	private INode next;
+	
+	public MyNodes(K key) {
+		this.key = key;
+		this.next = null;
+	}
+	
+	public INode getNext() {
+		return next;
+	}
+	
+	public void setNext(INode next) {
+		this.next = next;
+	}
+
+	@Override
+	public K getKey() {
+		return key;
+	}
+
+	@Override
+	public void setKey(K key) {
+		this.key = key;		
+	}
+}
+
